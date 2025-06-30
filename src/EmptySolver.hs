828d6fff618c43ee65d85ba01690dcefc4631f20
Mate Soos <mate.soos@argot.org>
2025-06-30 19:27:04 +0200

    Update

--- src/EmptySolver.hs
+++ src/EmptySolver.hs
@@ -13,6 +13,9 @@ main = do
           _ | "(set-option" `isPrefixOf` line -> putStrLn "success"
           _ | "(set-info" `isPrefixOf` line -> putStrLn "success"
           _ | "(set-logic" `isPrefixOf` line -> putStrLn "success"
+          _ | "(define" `isPrefixOf` line -> putStrLn "success"
+          _ | "(declare" `isPrefixOf` line -> putStrLn "success"
+          _ | "(assert" `isPrefixOf` line -> putStrLn "success"
           _ | "(reset)" `isPrefixOf` line -> putStrLn "success"
             | otherwise -> return ()
       hFlush stdout
