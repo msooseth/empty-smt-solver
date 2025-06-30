c77332279c041b3ce3931671ce5d1a3149d03b5d
Mate Soos <mate.soos@argot.org>
2025-06-30 19:25:35 +0200

    Update

--- src/EmptySolver.hs
+++ src/EmptySolver.hs
@@ -2,7 +2,9 @@ import Data.List (isPrefixOf)
 import System.IO
 
 main :: IO ()
-main = loop
+main = do
+  hSetBuffering stdout NoBuffering
+  loop
   where
     loop = do
       line <- getLine
@@ -10,6 +12,7 @@ main = loop
           "(check-sat)" -> putStrLn "unknown"
           _ | "(set-option" `isPrefixOf` line -> putStrLn "success"
           _ | "(set-info" `isPrefixOf` line -> putStrLn "success"
+          _ | "(set-logic" `isPrefixOf` line -> putStrLn "success"
           _ | "(reset)" `isPrefixOf` line -> putStrLn "success"
             | otherwise -> return ()
       hFlush stdout
